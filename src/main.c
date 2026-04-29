fe0d9dd606ff7b8da001656598f1c510872787fb
Person <localboxcrox@gmail.com>
2026-04-29 16:43:30 -0400

    stuff

--- src/main.c
+++ src/main.c
@@ -5,13 +5,14 @@
 #include "hart.h"
 #include "conv.h"
 
+static u64 uart_lock = new_spinlock();
+
 static noreturn void task1(void)
 {
-    static u64 lock = new_spinlock();
-    spin2lock(&lock);
+    spin2lock(&uart_lock);
     say_hart(uart0);
     uart_puts(uart0, "hi!\r\n");
-    spin2unlock(&lock);
+    spin2unlock(&uart_lock);
     hart_done();
 }
 
@@ -28,6 +29,9 @@ noreturn void main(void)
     // hi
     say_hart(uart0);
     uart_puts(uart0, "hello metal!\r\n");
+
+    say_hart(uart0);
+    uart_puts(uart0, "TASK TESTING HARTS: 2, 3\r\n");
 
     say_hart(uart0);
     uart_puts(uart0, "hart 0 hcb: ");
@@ -47,10 +51,12 @@ noreturn void main(void)
     uart_puts(uart0, "\r\n");
 
     say_hart(uart0);
-    uart_puts(uart0, "setting task for other hart 2 and 3...\r\n");
+    uart_puts(uart0,
+              "setting task for other harts 2 and 3 then 2 again...\r\n");
 
     hart_task(2, 0, 0, 0, 0, 0, 0, (addr)task1);
     hart_task(3, 0, 0, 0, 0, 0, 0, (addr)task1);
+    hart_task(2, 0, 0, 0, 0, 0, 0, (addr)task1);
 
     dead_spin();
 }
