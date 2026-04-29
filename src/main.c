afb5c1168859e0bd892fc0daf0cb09ff059b4cec
Person <localboxcrox@gmail.com>
2026-04-29 14:45:06 -0400

    stuff

--- src/main.c
+++ src/main.c
@@ -31,41 +31,14 @@ noreturn void main(void)
     uart_puts(uart0, lig);
     uart_puts(uart0, "\r\n");
 
-    volatile HCB *hcb = (volatile HCB *)M_get_HCB_addr(1);
-
     uart_puts(uart0, "hart 1 hcb: ");
-    uint2cstr(64, hcb, lig2);
+    uint2cstr(64, M_get_HCB_addr(1), lig2);
     uart_puts(uart0, lig2);
     uart_puts(uart0, "\r\n");
 
     uart_puts(uart0, "setting task for other hart 1...\r\n");
 
-    uart_puts(uart0, "pre: ");
-    uint2cstr(64, hcb->jump_addr, lig3);
-    uart_puts(uart0, lig3);
-    uart_puts(uart0, "\r\n");
-
     hart_task(1, 0, 0, 0, 0, 0, 0, (addr)task1);
 
-    // spin2lock(&hcb->lock);
-    // hcb->jump_addr = (addr)task1;
-    // spin2unlock(&hcb->lock);
-
-    uart_puts(uart0, "adr: ");
-    uint2cstr(64, (addr)task1, lig5);
-    uart_puts(uart0, lig5);
-    uart_puts(uart0, "\r\n");
-
-    uart_puts(uart0, "post: ");
-    uint2cstr(64, hcb->jump_addr, lig4);
-    uart_puts(uart0, lig4);
-    uart_puts(uart0, "\r\n");
-
-    // for (;;) {
-    //     uart_puts(uart0, "post: ");
-    //     uint2cstr(64, hcb->jump_addr, lig6);
-    //     uart_puts(uart0, lig6);
-    //     uart_puts(uart0, "\r\n");
-    // }
     dead_spin();
 }
