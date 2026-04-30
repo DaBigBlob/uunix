31f85e7fe7098844120f7a81dea91da4e26973d7
Person <localboxcrox@gmail.com>
2026-04-30 00:44:35 -0400

    stuff

--- src/std.h
+++ src/std.h
@@ -30,9 +30,9 @@ static const char HEX_CHARS[] = "0123456789abcdef";
             *cur = HEX_CHARS[num & 0xf];                                  \
             num >>= 4;                                                    \
         }                                                                 \
-        for (; (*id == '0' && *(id + 3)); ++id)                           \
+        for (; (*(id + 2) == '0' && *(id + 3)); ++id)                     \
             ;                                                             \
-        id[1] = 'x'                                                       \
+        id[1] = 'x';                                                      \
     } while (0)
 
 #define uint2cstr(size, _num, id)                                         \
