6f6098a778f66d006d90d71fd4f6ba75d777ee08
Person <localboxcrox@gmail.com>
2026-05-05 14:05:44 -0400

    stuff

--- src/hart.h
+++ src/hart.h
@@ -28,22 +28,19 @@ For each hart:
     - repeat
 */
 
-/* things a hart might want to store */
-typedef struct {
-    usize mstatus;
-} TEMP_hart;
-
 /* hart-specific control block */
 typedef struct {
-    usize     a0;
-    usize     a1;
-    usize     a2;
-    usize     a3;
-    usize     a4;
-    usize     a5;
-    usize     lock;
-    addr      jump_addr;
-    TEMP_hart store;
+    usize a0;
+    usize a1;
+    usize a2;
+    usize a3;
+    usize a4;
+    usize a5;
+    usize lock;
+    addr  jump_addr;
+    struct { /* things a hart might want to store */
+        usize mstatus;
+    } store;
 } HCB;
 
 #define M_get_HCB_addr(hartid)                                            \
