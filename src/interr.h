e05755a56592931a3364a3e8c8b40b57e232c554
Person <localboxcrox@gmail.com>
2026-05-05 13:56:39 -0400

    stuff

---
+++ src/interr.h
@@ -0,0 +1,8 @@
+#ifndef UUNIX_INTERR
+#define UUNIX_INTERR
+
+#include "base.h"
+
+#define MSTATUS_MIE ((usize)1u << 3)
+
+#endif // UUNIX_INTERR
--- src/lock.h
+++ src/lock.h
@@ -3,10 +3,12 @@
 
 #include "base.h"
 
-#define spin2unlock(lock) strict_swap((lock), 0)
 #define spin2lock(lock)                                                   \
     do {                                                                  \
     } while (strict_swap((lock), 077777777) != 0)
+
+#define spin2unlock(lock) strict_swap((lock), 0)
+
 #define new_spinlock() ((u64)(0))
 
 #endif // UUNIX_LOCK
