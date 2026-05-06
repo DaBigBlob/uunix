e2d1046e1f099232d7fd909cfe1bbdc570475300
Person <localboxcrox@gmail.com>
2026-05-06 00:43:21 -0400

    get rid of dirt

--- src/trap.h
+++ src/trap.h
@@ -22,10 +22,4 @@ extern volatile u32 clint[];
 #define MASK_MIE_MTIE ((usize)1 << 7)  /* timer interrupt enable */
 #define MASK_MIE_MEIE ((usize)1 << 11) /* external interrupt enable */
 
-#define MIE_storeNunset(hartid)                                           \
-    do {                                                                  \
-        store_mstatus(hartid);                                            \
-        set_mstatus(get_mstatus() & (~MASK_MSTATUS_MIE));                 \
-    } while (0)
-
 #endif // UUNIX_INTERR
