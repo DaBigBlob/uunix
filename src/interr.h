73f6f30e443232756ed62ac1f0c98846e5790bd2
Person <localboxcrox@gmail.com>
2026-05-05 21:20:16 -0400

    stuff

--- src/interr.h
+++ src/interr.h
@@ -1,9 +1,20 @@
 #ifndef UUNIX_INTERR
 #define UUNIX_INTERR
 
+#include "pre.h"
 #include "hart.h"
 
-extern volatile u8 clint[];
+extern volatile u32 clint[];
+
+#define get_msip(hartid) (clint[hartid] & 1)
+#define set_msip(hartid)                                                  \
+    do {                                                                  \
+        clint[hartid] = 1;                                                \
+    } while (0)
+#define unset_msip(hartid)                                                \
+    do {                                                                  \
+        clint[hartid] = 0;                                                \
+    } while (0)
 
 #define MSTATUS_MIE ((usize)1u << 3)
 
