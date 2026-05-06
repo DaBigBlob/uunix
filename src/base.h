1ae972e185f5b04cf4e2ed8a753ad2d3e750b0fa
Person <localboxcrox@gmail.com>
2026-05-05 22:26:18 -0400

    stuff

--- src/base.h
+++ src/base.h
@@ -57,7 +57,7 @@ UNIDEF(noreturn void, hart_begin,
        (usize a0, usize a1, usize a2, usize a3, usize a4, usize a5,
         usize sp, any jump_addr));
 
-UNIDEF(u64, wait2int, (void));
+UNIDEF(u64, wait4int, (void));
 
 UNIDEF(volatile addr, bss_begin, []);
 UNIDEF(volatile addr, bss_end, []);
