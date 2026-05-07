d0bdd970df44dad7661bd24991c233078f5b2389
Person <localboxcrox@gmail.com>
2026-05-06 21:05:24 -0400

    get rid of dirt

--- src/main.c
+++ src/main.c
@@ -2,27 +2,49 @@
 #include "base.h"
 #include "trap.h"
 #include "mem.h"
-#include "std.h"
+#include "uart.h"
 
 noreturn void main(void)
 {
-    volatile static u8 init_done = 0; // only hart 0 should mutate
-    /* inits hart 0 needs to do */
-    if (get_mhartid() == 0) {
-        mem_set(bss_begin, bss_end, volatile u8, 0);
-        uart_init(uart0);
-        uart_init(uart1);
-        init_done = 1;
-    } else {
-        while (!init_done)
-            ; // dumb wait: stack/memory not safe
+    /* pre mem touch begin: unsafe to touch memory ***********************/
+    {
+        /*
+        * Situation:
+            We don't want to touch stack yet because its probably in .bss
+            (which is what we are trying to zero).
+        * Remark:
+            Compiler should use registers only for this section but we
+            can't guarantee that from C.
+        * Solution:
+            We try our best to convince the compiler to put the variables
+            in this section in .data by assigning values to them.
+            'volatile static' should prevent further optimization attempts.
+        */
+        volatile static u8 init_pending = 0xf; // only hart 0 should mutate
+        /* inits hart 0 needs to do */
+        if (get_mhartid() == 0) {
+            {
+                volatile static u8 *_b = (u8 *)0xa;
+                for (_b = bss_begin; _b < bss_end; ++_b) {
+                    *_b = 0;
+                }
+            }
+            init_pending = 1;
+        } else {
+            while (init_pending)
+                ; // dumb wait: stack/memory not safe
+        }
     }
+    /* pre mem touch end: unsafe to touch memory *************************/
 
-    // int still disabled
-    /*********************************************************************/
     set_mstatus(get_mstatus() & ~MASK_MSTATUS_MIE); // disable int
 
     usize hartid = get_mhartid();
+
+    if (hartid == 0) {
+        uart_init(uart0);
+        uart_init(uart1);
+    }
 
     /* setup int */
     set_mtvec((any)trap_entry); // set int vec
@@ -36,9 +58,6 @@ noreturn void main(void)
 
     // int still disabled
     /*********************************************************************/
-
-    while (!init_done)
-        ; // dumb wait: nothing else safe yet
 
     if (hartid == 0) {
         // command
