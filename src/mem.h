6ac91bacf2665ae4aff1cef29dc37ec6ece048f7
Person <localboxcrox@gmail.com>
2026-05-06 21:17:54 -0400

    get rid of dirt

--- src/mem.h
+++ src/mem.h
@@ -19,6 +19,11 @@ typedef struct {
 
 extern hart_mem_t kern_mem[MAX_HARTS];
 
+_Static_assert((HART_STACK_SIZE % 16) == 0,
+               "hart stack size must be align 16");
+_Static_assert((sizeof(hart_mem_t) % 16) == 0,
+               "hart_mem_t size must be align 16");
+
 /* from linker */
 extern volatile u8 bss_begin[], bss_end[];
 
