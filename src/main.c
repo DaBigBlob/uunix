872418bb0deceef0070ba6f745e63989bf14d1c9
Person <localboxcrox@gmail.com>
2026-05-06 23:57:55 -0400

    get rid of dirt

--- src/main.c
+++ src/main.c
@@ -6,37 +6,6 @@
 
 noreturn void main(void)
 {
-    /* pre mem touch begin: unsafe to touch memory ***********************/
-    {
-        /*
-        * Situation:
-            We don't want to touch stack yet because its probably in .bss
-            (which is what we are trying to zero).
-        * Remark:
-            Compiler should use registers only for this section but we
-            can't guarantee that from C.
-        * Solution:
-            We try our best to convince the compiler to put the variables
-            in this section in .data by assigning values to them.
-            'volatile static' should prevent further optimization attempts.
-        */
-        volatile static u8 init_pending = 0xf; // only hart 0 should mutate
-        /* inits hart 0 needs to do */
-        if (get_mhartid() == 0) {
-            {
-                volatile static u8 *_b = (u8 *)0xa;
-                for (_b = bss_begin; _b < bss_end; ++_b) {
-                    *_b = 0;
-                }
-            }
-            init_pending = 0;
-        } else {
-            while (init_pending)
-                ; // dumb wait: stack/memory not safe
-        }
-    }
-    /* pre mem touch end: unsafe to touch memory *************************/
-
     set_mstatus(get_mstatus() & ~MASK_MSTATUS_MIE); // disable int
 
     usize hartid = get_mhartid();
